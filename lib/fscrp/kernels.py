e4788652eba020d64feac401f4422dba7b890af9
Andrew Roth <andrewjlroth@gmail.com>
2017-03-17 09:49:52 +0000

    refactor: separate log_q computation in prep for pg

--- lib/fscrp/kernels.py
+++ lib/fscrp/kernels.py
@@ -16,16 +16,16 @@ from pydp.utils import log_sum_exp
 
 class Kernel(object):
 
-    def __init__(self, alpha, log_likelihood_func, node_param_proposal_func, node_agg_func, node_param_prior_func):
+    def __init__(self, alpha, log_likelihood_func, node_param_proposal, node_agg_func, node_param_prior_func):
         self.alpha = alpha
 
-        self._log_likelihood_func = log_likelihood_func
+        self.log_likelihood_func = log_likelihood_func
 
-        self._node_param_proposal_func = node_param_proposal_func
+        self.node_param_proposal = node_param_proposal
 
-        self._node_agg_func = node_agg_func
+        self.node_agg_func = node_agg_func
 
-        self._node_param_prior_func = node_param_prior_func
+        self.node_param_prior_func = node_param_prior_func
 
     def propose_particle(self, data_point, parent_particle, seed=None):
         if seed is not None:
@@ -33,9 +33,11 @@ class Kernel(object):
 
             random.seed(seed)
 
-        log_q, node = self._propose_node(data_point, parent_particle)
+        node = self.propose_node(data_point, parent_particle)
 
-        log_p = self._log_likelihood_func(data_point, node.agg_params)
+        log_p = self.log_likelihood_func(data_point, node.agg_params)
+
+        log_q = self.get_log_q(data_point, node, parent_particle)
 
         log_w = self._compute_log_weight(node, parent_particle, log_q, log_p)
 
@@ -70,7 +72,7 @@ class Kernel(object):
 
             nodes.add(node)
 
-            log_weight += self._node_param_prior_func(node, nodes)
+            log_weight += self.node_param_prior_func(node, nodes)
 
         elif num_data_points > 1:
             log_weight += log(num_data_points - 1)
@@ -84,10 +86,22 @@ class Kernel(object):
 
 class BootstrapKernel(Kernel):
 
-    def _propose_node(self, data_point, parent_particle):
+    def get_log_q(self, data_point, node, parent_particle):
         nodes = get_nodes(parent_particle)
 
-        if len(nodes) == 0:
+        if node in nodes:
+            log_q = self._get_log_q_add_existing(data_point, node, parent_particle)
+
+        else:
+            log_q = self._get_log_q_add_new(data_point, node, parent_particle)
+
+        if parent_particle is not None:
+            log_q -= log(2)
+
+        return log_q
+
+    def propose_node(self, data_point, parent_particle):
+        if parent_particle is None:
             proposal_funcs = [self._add_data_point_to_new_node, ]
 
         else:
@@ -95,16 +109,16 @@ class BootstrapKernel(Kernel):
 
         proposal_func = random.choice(proposal_funcs)
 
-        log_q, node = proposal_func(data_point, parent_particle)
+        node = proposal_func(data_point, parent_particle)
 
-        return -log(len(proposal_funcs)) + log_q, node
+        return node
 
     def _add_data_point_to_existing_node(self, data_point, particle):
         nodes = get_nodes(particle)
 
         node = random.sample(nodes, 1)[0]
 
-        return -log(len(nodes)), node
+        return node
 
     def _add_data_point_to_new_node(self, data_point, particle):
         root_nodes = get_root_nodes(particle)
@@ -119,28 +133,54 @@ class BootstrapKernel(Kernel):
         else:
             children = []
 
-        node_params, log_q = self._node_param_proposal_func(data_point, get_node_params(particle))
+        node_params = self.node_param_proposal.propose(data_point, get_node_params(particle))
 
         subtree_params = [node_params, ]
 
         for child in children:
             subtree_params.append(child.agg_params)
 
-        agg_params = self._node_agg_func(subtree_params)
+        agg_params = self.node_agg_func(subtree_params)
 
         node = Node(tuple(children), node_params, agg_params)
 
+        return node
+
+    def _get_log_q_add_existing(self, data_point, node, parent_particle):
+        nodes = get_nodes(parent_particle)
+
+        return -log(len(nodes))
+
+    def _get_log_q_add_new(self, data_point, node, parent_particle):
+        log_q = self.node_param_proposal.log_p(node.node_params, data_point, get_node_params(parent_particle))
+
+        root_nodes = get_root_nodes(parent_particle)
+
+        num_root = len(root_nodes)
+
         log_q -= num_root * log(2)
 
-        return log_q, node
+        return log_q
 
 
 class SemiAdaptedKernel(Kernel):
 
-    def _propose_node(self, data_point, parent_particle):
+    def get_log_q(self, data_point, node, parent_particle):
         nodes = get_nodes(parent_particle)
 
-        if len(nodes) == 0:
+        if node in nodes:
+            log_q = self._get_log_q_add_existing(data_point, node, parent_particle)
+
+        else:
+            log_q = self._get_log_q_add_new(data_point, node, parent_particle)
+
+        if parent_particle is not None:
+            log_q -= log(2)
+
+        return log_q
+
+    def propose_node(self, data_point, parent_particle):
+        if parent_particle is None:
             proposal_funcs = [self._add_data_point_to_new_node, ]
 
         else:
@@ -148,9 +188,9 @@ class SemiAdaptedKernel(Kernel):
 
         proposal_func = random.choice(proposal_funcs)
 
-        log_q, node = proposal_func(data_point, parent_particle)
+        node = proposal_func(data_point, parent_particle)
 
-        return -log(len(proposal_funcs)) + log_q, node
+        return node
 
     def _add_data_point_to_existing_node(self, data_point, particle):
         nodes = list(get_nodes(particle))
@@ -160,7 +200,7 @@ class SemiAdaptedKernel(Kernel):
         log_p = []
 
         for node in nodes:
-            log_p.append(log(cluster_sizes[node]) + self._log_likelihood_func(data_point, node.agg_params))
+            log_p.append(log(cluster_sizes[node]) + self.log_likelihood_func(data_point, node.agg_params))
 
         p = exp_normalize(log_p)
 
@@ -168,9 +208,7 @@ class SemiAdaptedKernel(Kernel):
 
         node = nodes[node_idx]
 
-        log_q = log_p[node_idx] - log_sum_exp(log_p)
-
-        return log_q, node
+        return node
 
     def _add_data_point_to_new_node(self, data_point, particle):
         root_nodes = get_root_nodes(particle)
@@ -185,17 +223,44 @@ class SemiAdaptedKernel(Kernel):
         else:
             children = []
 
-        node_params, log_q = self._node_param_proposal_func(data_point, get_node_params(particle))
+        node_params = self.node_param_proposal.propose(data_point, get_node_params(particle))
 
         subtree_params = [node_params, ]
 
         for child in children:
             subtree_params.append(child.agg_params)
 
-        agg_params = self._node_agg_func(subtree_params)
+        agg_params = self.node_agg_func(subtree_params)
 
         node = Node(tuple(children), node_params, agg_params)
 
+        return node
+
+    def _get_log_q_add_existing(self, data_point, node, parent_particle):
+        nodes = list(get_nodes(parent_particle))
+
+        cluster_sizes = get_num_data_points_per_node(parent_particle)
+
+        log_p = []
+
+        for node in nodes:
+            log_p.append(log(cluster_sizes[node]) + self.log_likelihood_func(data_point, node.agg_params))
+
+        node_idx = nodes.index(node)
+
+        node = nodes[node_idx]
+
+        log_q = log_p[node_idx] - log_sum_exp(log_p)
+
+        return log_q
+
+    def _get_log_q_add_new(self, data_point, node, parent_particle):
+        log_q = self.node_param_proposal.log_p(node.node_params, data_point, get_node_params(parent_particle))
+
+        root_nodes = get_root_nodes(parent_particle)
+
+        num_root = len(root_nodes)
+
         log_q -= num_root * log(2)
 
-        return log_q, node
+        return log_q
