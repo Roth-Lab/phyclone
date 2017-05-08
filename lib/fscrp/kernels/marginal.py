4abe7a7e9b224831a87c8067230e9422a3eb27f8
Andrew Roth <andrewjlroth@gmail.com>
2017-05-08 16:31:01 +0100

    marignal code

---
+++ lib/fscrp/kernels/marginal.py
@@ -0,0 +1,129 @@
+'''
+Created on 8 May 2017
+
+@author: Andrew Roth
+'''
+from math import log
+from pydp.rvs import discrete_rvs
+
+import random
+
+from fscrp.data_structures import MarginalNode, MarginalParticle
+from fscrp.kernels.base import Kernel
+from fscrp.particle_utils import get_nodes, get_num_data_points_per_node, get_root_nodes
+from fscrp.utils import exp_normalize
+from pydp.utils import log_sum_exp
+
+
+class MarginalKernel(Kernel):
+
+    def __init__(self, alpha, grid_size=101):
+        self.alpha = alpha
+
+        self.grid_size = grid_size
+
+    def create_particle(self, data_point, node, parent_particle):
+        log_q = self._get_log_q(data_point, parent_particle)
+
+        log_w = log_sum_exp(log_q.values())
+
+        new_root_nodes = list(get_root_nodes(parent_particle))
+
+        [new_root_nodes.remove(x) for x in node.children if x in new_root_nodes]
+
+        new_root_nodes.append(node)
+
+        tree_log_p = self._get_tree_log_p(new_root_nodes)
+
+        return MarginalParticle(log_w=log_w, node=node, parent_particle=parent_particle, tree_log_p=tree_log_p)
+
+    def propose_node(self, data_point, parent_particle):
+        log_q = self._get_log_q(data_point, parent_particle)
+
+        node_probs = exp_normalize(log_q.values())
+
+        node_idx = discrete_rvs(node_probs)
+
+        node = log_q.keys()[node_idx]
+
+        node.add_data_point(data_point)
+
+        return node
+
+    def _get_log_q(self, data_point, parent_particle):
+        log_q = {}
+
+        log_q.update(self._add_data_point_to_new_node(data_point, parent_particle))
+
+        if parent_particle is not None:
+            log_q.update(self._add_data_point_to_existing_node(data_point, parent_particle))
+
+        return log_q
+
+    def _add_data_point_to_existing_node(self, data_point, parent_particle):
+        root_nodes = list(get_root_nodes(parent_particle))
+
+        cluster_sizes = get_num_data_points_per_node(parent_particle)
+
+        log_q = {}
+
+        for node in root_nodes:
+            node.add_data_point(data_point)
+
+            tree_log_p = self._get_tree_log_p(root_nodes)
+
+            log_q[node] = log(cluster_sizes[node]) + tree_log_p - parent_particle.tree_log_p
+
+            node.remove_data_point(data_point)
+
+        return log_q
+
+    def _add_data_point_to_new_node(self, data_point, parent_particle):
+        root_nodes = get_root_nodes(parent_particle)
+
+        num_root = len(root_nodes)
+
+        nodes = get_nodes(parent_particle)
+
+        num_clusters = len(nodes)
+
+        if num_clusters > 1:
+            log_p_forest = (num_clusters - 1) * log(num_clusters + 1) - num_clusters * log(num_clusters + 2)
+
+        else:
+            log_p_forest = 0
+
+        log_p_partition = log(self.alpha)
+
+        if parent_particle is None:
+            old_tree_log_p = 0
+
+        else:
+            old_tree_log_p = parent_particle.tree_log_p
+
+        log_q = {}
+
+        for num_children in range(0, num_root + 1):
+            #             for children in itertools.combinations(root_nodes, num_children):
+            children = random.sample(root_nodes, num_children)
+
+            node = MarginalNode(children)
+
+            node.add_data_point(data_point)
+
+            new_root_nodes = set(root_nodes) - set(children)
+
+            new_root_nodes.add(node)
+
+            new_root_nodes = tuple(new_root_nodes)
+
+            tree_log_p = self._get_tree_log_p(new_root_nodes)
+
+            log_q[node] = log_p_partition + log_p_forest + tree_log_p - old_tree_log_p
+
+            node.remove_data_point(data_point)
+
+        return log_q
+
+    def _get_tree_log_p(self, root_nodes):
+        return MarginalNode(root_nodes).log_p
---
+++ lib/fscrp/math_utils.py
@@ -0,0 +1,99 @@
+'''
+Created on 8 Dec 2016
+
+@author: Andrew Roth
+'''
+from __future__ import division
+
+import math
+import numba
+import numpy as np
+
+
+def discrete_rvs(p):
+    return np.random.multinomial(1, p).argmax()
+
+
+@numba.jit(cache=True, nopython=True)
+def exp_normalize(log_p):
+    log_norm = log_sum_exp(log_p)
+
+    p = np.exp(log_p - log_norm)
+
+    p = p / p.sum()
+
+    return p, log_norm
+
+
+@numba.jit(nopython=True)
+def log_sum_exp(log_X):
+    '''
+    Given a list of values in log space, log_X. Compute exp(log_X[0] + log_X[1] + ... log_X[n])
+
+    Numerically safer than naive method.
+    '''
+    max_exp = np.max(log_X)
+
+    if np.isinf(max_exp):
+        return max_exp
+
+    total = 0
+
+    for x in log_X:
+        total += np.exp(x - max_exp)
+
+    return np.log(total) + max_exp
+
+
+@numba.jit(cache=True, nopython=True)
+def log_normalize(log_p):
+    return log_p - log_sum_exp(log_p)
+
+
+@numba.jit(cache=True, nopython=True)
+def cholesky_update(L, x, alpha=1, inplace=True):
+    """ Rank one update of a Cholesky factorized matrix.
+    """
+    dim = len(x)
+
+    x = x.copy()
+
+    if not inplace:
+        L = L.copy()
+
+    for i in range(dim):
+        r = np.sqrt(L[i, i] ** 2 + alpha * x[i] ** 2)
+
+        c = r / L[i, i]
+
+        s = x[i] / L[i, i]
+
+        L[i, i] = r
+
+        idx = i + 1
+
+        L[idx:dim, i] = (L[idx:dim, i] + alpha * s * x[idx:dim]) / c
+
+        x[idx:dim] = c * x[idx:dim] - s * L[idx:dim, i]
+
+    return L
+
+
+@numba.jit(cache=True, nopython=True)
+def cholesky_log_det(X):
+    return 2 * np.sum(np.log(np.diag(X)))
+
+
+@numba.jit(cache=True, nopython=True)
+def log_factorial(x):
+    return log_gamma(x + 1)
+
+
+@numba.jit(cache=True, nopython=True)
+def log_binomial_coefficient(n, x):
+    return log_factorial(n) - log_factorial(x) - log_factorial(n - x)
+
+
+@numba.vectorize(["float64(float64)", "int64(float64)"])
+def log_gamma(x):
+    return math.lgamma(x)
