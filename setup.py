765b4854a28c25ccbac6e32abc273b4e239c7638
Andrew Roth <andrewjlroth@gmail.com>
2017-12-05 17:16:01 +0000

    reorganising

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import find_packages, setup
 
 setup(
     name='fscrp',
-    version='0.0.3',
-    description='A Python library for implementing for tree structured chinese restaurant process models.',
+    version='0.1.0',
+    description='A Python library implementing the forest structured chinese restaurant process models.',
     author='Andrew Roth',
     author_email='andrewjlroth@gmail.com',
     url='https://bitbucket.org/aroth85/fscrp',
-    package_dir={'': 'lib'},
     packages=find_packages(),
     license='GPL v3'
 )
