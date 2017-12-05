659c40063aea00653141bf913a7ba2713e897af5
Andrew Roth <andrewjlroth@gmail.com>
2017-12-05 17:50:47 +0000

    documenting swarm

--- fscrp/samplers/swarm.py
+++ fscrp/samplers/swarm.py
@@ -1,16 +1,16 @@
-'''
-Created on 16 Mar 2017
-
-@author: Andrew Roth
-'''
-from __future__ import division
-
-from fscrp.math_utils import log_sum_exp
+from __future__ import division, print_function
 
 import numpy as np
 
 
+from fscrp.math_utils import log_sum_exp
+
+
 class ParticleSwarm(object):
+    """ A collection of particles from one iterations of an SMC algorithm.
+
+    This class handles tracking particles, their weights and normalization.
+    """
 
     def __init__(self):
         self.particles = []
@@ -21,10 +21,14 @@ class ParticleSwarm(object):
 
     @property
     def ess(self):
+        """ Effective sample size of swarm.
+        """
         return 1 / np.sum(np.square(self.weights))
 
     @property
     def log_norm_const(self):
+        """ Log of the normalization constant i.e. log(Z).
+        """
         if self._log_norm_const is None:
             self._log_norm_const = log_sum_exp(self.unnormalized_log_weights)
 
@@ -32,22 +36,32 @@ class ParticleSwarm(object):
 
     @property
     def log_weights(self):
+        """ Normalized log weights of particles.
+        """
         return self.unnormalized_log_weights - self.log_norm_const
 
     @property
     def num_particles(self):
+        """ Number of particles in the swarm.
+        """
         return len(self.particles)
 
     @property
     def relative_ess(self):
+        """ ESS normalized to number of particles.
+        """
         return self.ess / self.num_particles
 
     @property
     def unnormalized_log_weights(self):
+        """ Raw log weights of particles.
+        """
         return np.array(self._unnormalized_log_weights)
 
     @property
     def weights(self):
+        """ Particle weights.
+        """
         weights = np.exp(self.log_weights)
 
         weights = weights / weights.sum()
@@ -55,21 +69,22 @@ class ParticleSwarm(object):
         return weights
 
     def add_particle(self, log_weight, particle):
-        '''
-        Args:
-            log_weight: Unnormalized log weight of particle
-            particle: Particle
-        '''
+        """ Add a particle to the swarm.
+
+        Parameters
+        ----------
+        log_weight: float
+            Unnormalized log weight of particle
+
+        particle: Particle
+        """
         self.particles.append(particle)
 
         self._unnormalized_log_weights.append(log_weight)
 
         self._log_norm_const = None
 
-    def to_dict(self):
-        #         result = defaultdict(float)
-        #
-        #         for p, w in zip(self.particles, self.weights):
-        #             result[p] += w
-
+    def to_list(self):
+        """ Return a list of two tuples where the first entry is a particle and the second is the weight.
+        """
         return zip(self.particles, self.weights)
