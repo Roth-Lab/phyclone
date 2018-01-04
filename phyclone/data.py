28f505c57f2ae3deab4ea6b9c13a47897bda73ba
Andrew Roth <andrewjlroth@gmail.com>
2018-01-04 17:39:36 +0000

    minor: reverting normalizing data point likelihoods

--- phyclone/data.py
+++ phyclone/data.py
@@ -1,20 +1,10 @@
-import numpy as np
-
-from phyclone.math_utils import log_normalize, log_sum_exp
-
-
 class DataPoint(object):
     __slots__ = ('idx', 'value', 'outlier_prob')
 
     def __init__(self, idx, value, outlier_prob=0):
         self.idx = idx
 
-        self.value = np.zeros(value.shape)
-
-        for i in range(value.shape[0]):
-            self.value[i, :] = log_normalize(value[i, :])
-
-            assert log_sum_exp(self.value[i]) == 0
+        self.value = value
 
         self.outlier_prob = outlier_prob
 
