120d9fae307dc4a52f7f7cb86795dc00155c82c2
Andrew Roth <aroth@bccrc.ca>
2021-11-22 10:39:46 -0800

    feature: misc utils

---
+++ phyclone/utils.py
@@ -0,0 +1,58 @@
+import numba
+import numpy as np
+import time
+
+
+def set_seed(seed):
+    if seed is not None:
+        np.random.seed(seed)
+
+        set_numba_seed(seed)
+
+
+@numba.jit
+def set_numba_seed(seed):
+    np.random.seed(seed)
+
+    
+class Timer:
+    """ Taken from https://www.safaribooksonline.com/library/view/python-cookbook-3rd/9781449357337/ch13s13.html
+    """
+
+    def __init__(self, func=time.time):
+        self.elapsed = 0.0
+
+        self._func = func
+
+        self._start = None
+
+    @property
+    def running(self):
+        return self._start is not None
+
+    def reset(self):
+        self.elapsed = 0.0
+
+    def start(self):
+        if self._start is not None:
+            raise RuntimeError('Already started')
+
+        self._start = self._func()
+
+    def stop(self):
+        if self._start is None:
+            raise RuntimeError('Not started')
+
+        end = self._func()
+
+        self.elapsed += end - self._start
+
+        self._start = None
+
+    def __enter__(self):
+        self.start()
+
+        return self
+
+    def __exit__(self, *args):
+        self.stop()
