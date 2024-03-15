ece4608099ab1732f22efdc2443eddfe97970fc5
elhurtado <74883583+elhurtado@users.noreply.github.com>
2024-03-15 16:41:39 -0700

    Refactor on FSCRPDistribution and TreeJointDistribution

--- phyclone/tree/distributions.py
+++ phyclone/tree/distributions.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 from phyclone.utils.math import log_factorial, log_sum_exp
 
 
@@ -9,15 +8,20 @@ class FSCRPDistribution(object):
     def __init__(self, alpha):
         self.alpha = alpha
 
-    def log_p(self, tree):
+    def log_p(self, tree, node_data=None):
+
+        if node_data is None:
+            node_data = tree.node_data
+
         log_p = 0
 
         # CRP prior
-        num_nodes = len(tree.nodes)
+        # num_nodes = len(tree.nodes)
+        num_nodes = tree.get_number_of_nodes()
 
         log_p += num_nodes * np.log(self.alpha)
 
-        for node, node_data in tree.node_data.items():
+        for node, node_data in node_data.items():
             if node == -1:
                 continue
 
@@ -37,19 +41,15 @@ class TreeJointDistribution(object):
 
     def log_p(self, tree):
         """The log likelihood of the data marginalized over root node parameters."""
-        log_p = self.prior.log_p(tree)
+
+        node_data = tree.node_data
+
+        log_p = self.prior.log_p(tree, node_data)
 
         # Outlier prior
-        for node, node_data in tree.node_data.items():
-            for data_point in node_data:
-                if data_point.outlier_prob != 0:
-                    if node == -1:
-                        log_p += data_point.outlier_prob
+        log_p = self.outlier_prior(log_p, node_data)
 
-                    else:
-                        log_p += data_point.outlier_prob_not
-
-        if len(tree.roots) > 0:
+        if tree.get_number_of_children("root") > 0:
             for i in range(tree.grid_size[0]):
                 log_p += log_sum_exp(tree.data_log_likelihood[i, :])
 
@@ -60,10 +60,26 @@ class TreeJointDistribution(object):
 
     def log_p_one(self, tree):
         """The log likelihood of the data conditioned on the root having value 1.0 in all dimensions."""
-        log_p = self.prior.log_p(tree)
+
+        node_data = tree.node_data
+
+        log_p = self.prior.log_p(tree, node_data)
 
         # Outlier prior
-        for node, node_data in tree.node_data.items():
+        log_p = self.outlier_prior(log_p, node_data)
+
+        if tree.get_number_of_children("root") > 0:
+            for i in range(tree.grid_size[0]):
+                log_p += tree.data_log_likelihood[i, -1]
+
+        for data_point in tree.outliers:
+            log_p += data_point.outlier_marginal_prob
+
+        return log_p
+
+    @staticmethod
+    def outlier_prior(log_p, node_data):
+        for node, node_data in node_data.items():
             for data_point in node_data:
                 if data_point.outlier_prob != 0:
                     if node == -1:
@@ -71,12 +87,4 @@ class TreeJointDistribution(object):
 
                     else:
                         log_p += data_point.outlier_prob_not
-
-        if len(tree.roots) > 0:
-            for i in range(tree.grid_size[0]):
-                log_p += tree.data_log_likelihood[i, -1]
-
-        for data_point in tree.outliers:
-            log_p += data_point.outlier_marginal_prob
-
         return log_p
