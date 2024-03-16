a443690c6a26be3687d823043e8bb31fe7767857
elhurtado <74883583+elhurtado@users.noreply.github.com>
2024-03-15 17:43:23 -0700

    Reducing redundant work in PruneRegraphSampler

--- phyclone/mcmc/gibbs_mh.py
+++ phyclone/mcmc/gibbs_mh.py
@@ -75,28 +75,18 @@ class PruneRegraphSampler(object):
 
         self._rng = rng
 
-        # self.labeled = labeled
-
     def sample_tree(self, tree):
         if tree.get_number_of_nodes() <= 1:
             return tree
 
-        remaining_nodes, subtree_root = self._get_subtree_and_pruned_tree(tree)
+        remaining_nodes, pruned_tree, subtree = self._get_subtree_and_pruned_tree(tree)
 
         if len(remaining_nodes) == 0:
             return tree
 
-        trees = self._create_sampled_trees_array(remaining_nodes, subtree_root, tree)
+        trees = self._create_sampled_trees_array(remaining_nodes, pruned_tree, subtree)
 
         log_p = np.array([np.log(n + 1) + self.tree_dist.log_p_one(x) for n, x in trees])
-
-        # if self.labeled:
-        #     log_p = np.array(
-        #         [np.log(n + 1) + self.tree_dist.log_p_one(x) for n, x in trees]
-        #     )
-        #
-        # else:
-        #     log_p = np.array([self.tree_dist.log_p_one(x) for _, x in trees])
 
         p, _ = exp_normalize(log_p)
 
@@ -105,16 +95,16 @@ class PruneRegraphSampler(object):
         return trees[idx][1]
 
     @staticmethod
-    def _create_sampled_trees_array(remaining_nodes, subtree_root, tree):
+    def _create_sampled_trees_array(remaining_nodes, pruned_tree, subtree):
         trees = []
         # Descendant from dummy normal node
         remaining_nodes.append(None)
         for parent in remaining_nodes:
-            new_tree = tree.copy()
+            new_tree = pruned_tree.copy()
 
-            subtree = new_tree.get_subtree(subtree_root)
-
-            new_tree.remove_subtree(subtree)
+            # subtree = new_tree.get_subtree(subtree_root)
+            #
+            # new_tree.remove_subtree(subtree)
 
             if parent is None:
                 # nc = len(new_tree.roots)
@@ -132,9 +122,71 @@ class PruneRegraphSampler(object):
         return trees
 
     def _get_subtree_and_pruned_tree(self, tree):
-        new_tree = tree.copy()
-        subtree_root = self._rng.choice(new_tree.nodes)
-        subtree = new_tree.get_subtree(subtree_root)
-        new_tree.remove_subtree(subtree)
-        remaining_nodes = new_tree.nodes
-        return remaining_nodes, subtree_root
+        pruned_tree = tree.copy()
+        subtree_root = self._rng.choice(pruned_tree.nodes)
+        subtree = pruned_tree.get_subtree(subtree_root)
+        pruned_tree.remove_subtree(subtree)
+        remaining_nodes = pruned_tree.nodes
+        return remaining_nodes, pruned_tree, subtree
+
+# class PruneRegraphSampler(object):
+#     """Prune a subtree and regraph by Gibbs sampling possible attachement points"""
+#
+#     def __init__(self, tree_dist, rng: np.random.Generator):
+#         self.tree_dist = tree_dist
+#
+#         self._rng = rng
+#
+#     def sample_tree(self, tree):
+#         if tree.get_number_of_nodes() <= 1:
+#             return tree
+#
+#         remaining_nodes, subtree_root = self._get_subtree_and_pruned_tree(tree)
+#
+#         if len(remaining_nodes) == 0:
+#             return tree
+#
+#         trees = self._create_sampled_trees_array(remaining_nodes, subtree_root, tree)
+#
+#         log_p = np.array([np.log(n + 1) + self.tree_dist.log_p_one(x) for n, x in trees])
+#
+#         p, _ = exp_normalize(log_p)
+#
+#         idx = discrete_rvs(p, self._rng)
+#
+#         return trees[idx][1]
+#
+#     @staticmethod
+#     def _create_sampled_trees_array(remaining_nodes, subtree_root, tree):
+#         trees = []
+#         # Descendant from dummy normal node
+#         remaining_nodes.append(None)
+#         for parent in remaining_nodes:
+#             new_tree = tree.copy()
+#
+#             subtree = new_tree.get_subtree(subtree_root)
+#
+#             new_tree.remove_subtree(subtree)
+#
+#             if parent is None:
+#                 # nc = len(new_tree.roots)
+#                 nc = new_tree.get_number_of_children("root")
+#
+#             else:
+#                 # nc = len(new_tree.get_children(parent))
+#                 nc = new_tree.get_number_of_children(parent)
+#
+#             new_tree.add_subtree(subtree, parent=parent)
+#
+#             new_tree.update()
+#
+#             trees.append([nc, new_tree])
+#         return trees
+#
+#     def _get_subtree_and_pruned_tree(self, tree):
+#         new_tree = tree.copy()
+#         subtree_root = self._rng.choice(new_tree.nodes)
+#         subtree = new_tree.get_subtree(subtree_root)
+#         new_tree.remove_subtree(subtree)
+#         remaining_nodes = new_tree.nodes
+#         return remaining_nodes, subtree_root
