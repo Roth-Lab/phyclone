4c50082c5cde3b6ec08a79e658d2ebe3493837d8
elhurtado <74883583+elhurtado@users.noreply.github.com>
2024-04-01 22:43:00 -0700

    pyproject.toml file created, setup.py simplified

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name='PhyClone',
-    version='0.2.0',
-    description='A method for inferring clonal phylogenies from SNV data.',
-    author='Andrew Roth',
-    author_email='andrewjlroth@gmail.com',
-    url='https://bitbucket.org/aroth85/phyclone',
-    packages=find_packages(),
-    license='GPL v3',
-    entry_points={
-        'console_scripts': [
-            'phyclone = phyclone.cli:main',
-        ]
-    }
-)
+setup()
