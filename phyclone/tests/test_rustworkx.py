658b6cd5d3dfd52b0aa812ca02d7221775325f5a
elhurtado <74883583+elhurtado@users.noreply.github.com>
2024-05-17 10:41:57 -0700

    Updating rustworkx tests

--- phyclone/tests/test_rustworkx.py
+++ phyclone/tests/test_rustworkx.py
@@ -78,70 +78,6 @@ class Test(unittest.TestCase):
         self.assertEqual(copied_tree, original_tree)
         self.assertIsNot(copied_tree, original_tree)
 
-    # def test_cherry_tree_to_dict_representation_v2(self):
-    #
-    #     data = self.build_six_datapoints()
-    #
-    #     expected_tree = self.build_cherry_tree(Tree, data)
-    #
-    #     actual_tree = self.build_cherry_tree(Tree, data)
-    #
-    #     actual_dict = actual_tree.to_dict()
-    #
-    #     expected_dict = expected_tree.to_dict_old()
-    #
-    #     expected_tree2 = Tree.from_dict_old(data, expected_dict)
-    #
-    #     actual_tree2 = Tree.from_dict(data, actual_dict)
-    #
-    #     self.assertEqual(actual_tree2, expected_tree)
-    #     self.assertEqual(actual_tree2, actual_tree)
-    #     self.assertEqual(actual_tree2, expected_tree2)
-
-    def test_single_node_tree_to_dict_representation(self):
-        n = 100
-        p = 1.0
-
-        data = self._create_data_points(4, n, p)
-
-        actual_tree = Tree.get_single_node_tree(data)
-
-        expected_tree = OldTree.get_single_node_tree(data)
-
-        actual_dict = actual_tree.to_dict()
-
-        expected_dict = expected_tree.to_dict()
-
-        self.assertDictEqual(actual_dict, expected_dict)
-
-    def test_cherry_tree_to_dict_representation(self):
-
-        expected_tree = self.build_cherry_tree(OldTree)
-
-        actual_tree = self.build_cherry_tree(Tree)
-
-        actual_dict = actual_tree.to_dict()
-
-        expected_dict = expected_tree.to_dict()
-
-        self.assertEqual(actual_tree, expected_tree)
-
-        self.assertDictEqual(actual_dict, expected_dict)
-
-    def test_linear_tree_to_dict_representation(self):
-
-        expected_tree = self.build_linear_tree(OldTree)
-
-        actual_tree = self.build_linear_tree(Tree)
-
-        actual_dict = actual_tree.to_dict()
-
-        expected_dict = expected_tree.to_dict()
-
-        self.assertEqual(actual_tree, expected_tree)
-
-        self.assertDictEqual(actual_dict, expected_dict)
-
     def test_single_node_tree_from_dict_representation(self):
         n = 100
         p = 1.0
@@ -150,22 +86,26 @@ class Test(unittest.TestCase):
 
         expected_tree = OldTree.get_single_node_tree(data)
 
-        expected_dict = expected_tree.to_dict()
+        actual_tree_built = Tree.get_single_node_tree(data)
 
-        actual_tree = Tree.from_dict(expected_dict)
+        actual_tree_dict = actual_tree_built.to_dict()
 
-        self.assertEqual(expected_tree, actual_tree)
+        actual_tree = Tree.from_dict(actual_tree_dict)
+
+        self.assertTrue(tree_eq(expected_tree, actual_tree))
 
     def test_cherry_tree_from_dict_representation(self):
         data = self.build_six_datapoints()
 
         expected_tree = self.build_cherry_tree(OldTree, data)
 
-        expected_dict = expected_tree.to_dict()
+        actual_tree_built = self.build_cherry_tree(Tree, data)
 
-        actual_tree = Tree.from_dict(expected_dict)
+        actual_tree_dict = actual_tree_built.to_dict()
 
-        self.assertEqual(expected_tree, actual_tree)
+        actual_tree = Tree.from_dict(actual_tree_dict)
+
+        self.assertTrue(tree_eq(expected_tree, actual_tree))
 
     def test_linear_tree_from_dict_representation(self):
 
@@ -173,99 +113,80 @@ class Test(unittest.TestCase):
 
         expected_tree = self.build_linear_tree(OldTree, data)
 
-        expected_dict = expected_tree.to_dict()
+        actual_tree_built = self.build_linear_tree(Tree, data)
 
-        actual_tree = Tree.from_dict(expected_dict)
+        actual_tree_dict = actual_tree_built.to_dict()
 
-        self.assertEqual(expected_tree, actual_tree)
+        actual_tree = Tree.from_dict(actual_tree_dict)
+
+        self.assertTrue(tree_eq(expected_tree, actual_tree))
 
     def test_getting_subtree_from_linear(self):
         data = self.build_six_datapoints()
 
-        expected_tree = OldTree.get_single_node_tree(data[:2])
+        expected_tree = self.build_linear_tree(OldTree, data)
 
-        expected_tree_roots = expected_tree.roots
+        actual_tree_built = self.build_linear_tree(Tree, data)
 
-        exp_n_1 = expected_tree.create_root_node(children=expected_tree_roots, data=data[2:4])
+        actual_tree_dict = actual_tree_built.to_dict()
 
-        expected_tree_roots = expected_tree.roots
+        actual_tree = Tree.from_dict(actual_tree_dict)
 
-        exp_n_2 = expected_tree.create_root_node(children=[exp_n_1], data=data[4:])
-
-        expected_dict = expected_tree.to_dict()
-
-        actual_tree = Tree.from_dict(expected_dict)
+        self.assertTrue(tree_eq(expected_tree, actual_tree))
 
         self.assertEqual(expected_tree, actual_tree)
 
-        subtree = actual_tree.get_subtree(exp_n_1)
+        actual_subtree = actual_tree.get_subtree(0)
 
-        exp_subtree = expected_tree.get_subtree(exp_n_1)
+        exp_subtree = expected_tree.get_subtree(0)
 
-        actual_dict2 = subtree.to_dict()
+        self.assertTrue(tree_eq(exp_subtree, actual_subtree))
 
-        expected_dict2 = exp_subtree.to_dict()
-
-        self.assertDictEqual(actual_dict2, expected_dict2)
 
     def test_getting_subtree_from_cherry(self):
         data = self.build_six_datapoints()
 
-        expected_tree = OldTree.get_single_node_tree(data[:2])
+        expected_tree = self.build_cherry_tree(OldTree, data)
 
-        exp_n_1 = expected_tree.create_root_node(children=[], data=data[2:4])
+        actual_tree_built = self.build_cherry_tree(Tree, data)
 
-        expected_tree_roots = expected_tree.roots
+        actual_tree_dict = actual_tree_built.to_dict()
 
-        exp_n_2 = expected_tree.create_root_node(children=expected_tree_roots, data=data[4:])
+        actual_tree = Tree.from_dict(actual_tree_dict)
 
-        expected_dict = expected_tree.to_dict()
-
-        actual_tree = Tree.from_dict(expected_dict)
+        self.assertTrue(tree_eq(expected_tree, actual_tree))
 
         self.assertEqual(expected_tree, actual_tree)
 
-        subtree = actual_tree.get_subtree(exp_n_2)
+        actual_subtree = actual_tree.get_subtree(1)
 
-        exp_subtree = expected_tree.get_subtree(exp_n_2)
+        exp_subtree = expected_tree.get_subtree(1)
 
-        actual_dict2 = subtree.to_dict()
-
-        expected_dict2 = exp_subtree.to_dict()
-
-        self.assertDictEqual(actual_dict2, expected_dict2)
+        self.assertTrue(tree_eq(exp_subtree, actual_subtree))
 
 
     def test_removing_subtree_from_linear(self):
         data = self.build_six_datapoints()
 
-        expected_tree = OldTree.get_single_node_tree(data[:2])
+        expected_tree = self.build_linear_tree(OldTree, data)
 
-        expected_tree_roots = expected_tree.roots
+        actual_tree_built = self.build_linear_tree(Tree, data)
 
-        exp_n_1 = expected_tree.create_root_node(children=expected_tree_roots, data=data[2:4])
+        actual_tree_dict = actual_tree_built.to_dict()
 
-        expected_tree_roots = expected_tree.roots
+        actual_tree = Tree.from_dict(actual_tree_dict)
 
-        exp_n_2 = expected_tree.create_root_node(children=[exp_n_1], data=data[4:])
-
-        expected_dict = expected_tree.to_dict()
-
-        actual_tree = Tree.from_dict(expected_dict)
+        self.assertTrue(tree_eq(expected_tree, actual_tree))
 
         self.assertEqual(expected_tree, actual_tree)
 
-        subtree = actual_tree.get_subtree(exp_n_1)
+        actual_subtree = actual_tree.get_subtree(0)
 
-        exp_subtree = expected_tree.get_subtree(exp_n_1)
+        exp_subtree = expected_tree.get_subtree(0)
 
-        actual_dict2 = subtree.to_dict()
+        self.assertTrue(tree_eq(exp_subtree, actual_subtree))
 
-        expected_dict2 = exp_subtree.to_dict()
-
-        self.assertDictEqual(actual_dict2, expected_dict2)
-
-        actual_tree.remove_subtree(subtree)
+        actual_tree.remove_subtree(actual_subtree)
 
         expected_tree.remove_subtree(exp_subtree)
 
@@ -275,31 +196,25 @@ class Test(unittest.TestCase):
     def test_removing_subtree_from_cherry(self):
         data = self.build_six_datapoints()
 
-        expected_tree = OldTree.get_single_node_tree(data[:2])
+        expected_tree = self.build_cherry_tree(OldTree, data)
 
-        exp_n_1 = expected_tree.create_root_node(children=[], data=data[2:4])
+        actual_tree_built = self.build_cherry_tree(Tree, data)
 
-        expected_tree_roots = expected_tree.roots
+        actual_tree_dict = actual_tree_built.to_dict()
 
-        exp_n_2 = expected_tree.create_root_node(children=expected_tree_roots, data=data[4:])
+        actual_tree = Tree.from_dict(actual_tree_dict)
 
-        expected_dict = expected_tree.to_dict()
-
-        actual_tree = Tree.from_dict(expected_dict)
+        self.assertTrue(tree_eq(expected_tree, actual_tree))
 
         self.assertEqual(expected_tree, actual_tree)
 
-        subtree = actual_tree.get_subtree(exp_n_2)
+        actual_subtree = actual_tree.get_subtree(1)
 
-        exp_subtree = expected_tree.get_subtree(exp_n_2)
+        exp_subtree = expected_tree.get_subtree(1)
 
-        actual_dict2 = subtree.to_dict()
+        self.assertTrue(tree_eq(exp_subtree, actual_subtree))
 
-        expected_dict2 = exp_subtree.to_dict()
-
-        self.assertDictEqual(actual_dict2, expected_dict2)
-
-        actual_tree.remove_subtree(subtree)
+        actual_tree.remove_subtree(actual_subtree)
 
         expected_tree.remove_subtree(exp_subtree)
 
@@ -308,78 +223,64 @@ class Test(unittest.TestCase):
     def test_adding_subtree_to_linear(self):
         data = self.build_six_datapoints()
 
-        expected_tree = OldTree.get_single_node_tree(data[:2])
+        expected_tree = self.build_linear_tree(OldTree, data)
 
-        expected_tree_roots = expected_tree.roots
+        actual_tree_built = self.build_linear_tree(Tree, data)
 
-        exp_n_1 = expected_tree.create_root_node(children=expected_tree_roots, data=data[2:4])
+        actual_tree_dict = actual_tree_built.to_dict()
 
-        expected_tree_roots = expected_tree.roots
+        actual_tree = Tree.from_dict(actual_tree_dict)
 
-        exp_n_2 = expected_tree.create_root_node(children=[exp_n_1], data=data[4:])
-
-        expected_dict = expected_tree.to_dict()
-
-        actual_tree = Tree.from_dict(expected_dict)
+        self.assertTrue(tree_eq(expected_tree, actual_tree))
 
         self.assertEqual(expected_tree, actual_tree)
 
-        subtree = actual_tree.get_subtree(exp_n_1)
+        actual_subtree = actual_tree.get_subtree(0)
 
-        exp_subtree = expected_tree.get_subtree(exp_n_1)
+        exp_subtree = expected_tree.get_subtree(0)
 
-        actual_dict2 = subtree.to_dict()
+        self.assertTrue(tree_eq(exp_subtree, actual_subtree))
 
-        expected_dict2 = exp_subtree.to_dict()
-
-        self.assertDictEqual(actual_dict2, expected_dict2)
-
-        actual_tree.remove_subtree(subtree)
+        actual_tree.remove_subtree(actual_subtree)
 
         expected_tree.remove_subtree(exp_subtree)
 
         self.assertTrue(tree_eq(actual_tree, expected_tree))
 
-        actual_tree.add_subtree(subtree, exp_n_2)
+        actual_tree.add_subtree(actual_subtree, 1)
 
-        expected_tree.add_subtree(exp_subtree, exp_n_2)
+        expected_tree.add_subtree(exp_subtree, 1)
 
         self.assertTrue(tree_eq(actual_tree, expected_tree))
 
     def test_adding_subtree_to_cherry(self):
         data = self.build_six_datapoints()
 
-        expected_tree = OldTree.get_single_node_tree(data[:2])
+        expected_tree = self.build_cherry_tree(OldTree, data)
 
-        exp_n_1 = expected_tree.create_root_node(children=[], data=data[2:4])
+        actual_tree_built = self.build_cherry_tree(Tree, data)
 
-        expected_tree_roots = expected_tree.roots
+        actual_tree_dict = actual_tree_built.to_dict()
 
-        exp_n_2 = expected_tree.create_root_node(children=expected_tree_roots, data=data[4:])
+        actual_tree = Tree.from_dict(actual_tree_dict)
 
-        expected_dict = expected_tree.to_dict()
-
-        actual_tree = Tree.from_dict(expected_dict)
+        self.assertTrue(tree_eq(expected_tree, actual_tree))
 
         self.assertEqual(expected_tree, actual_tree)
 
-        subtree = actual_tree.get_subtree(exp_n_2)
+        actual_subtree = actual_tree.get_subtree(1)
 
-        exp_subtree = expected_tree.get_subtree(exp_n_2)
+        exp_subtree = expected_tree.get_subtree(1)
 
-        actual_dict2 = subtree.to_dict()
+        self.assertTrue(tree_eq(exp_subtree, actual_subtree))
 
-        expected_dict2 = exp_subtree.to_dict()
-
-        self.assertDictEqual(actual_dict2, expected_dict2)
-
-        actual_tree.remove_subtree(subtree)
+        actual_tree.remove_subtree(actual_subtree)
 
         expected_tree.remove_subtree(exp_subtree)
 
         self.assertTrue(tree_eq(actual_tree, expected_tree))
 
-        actual_tree.add_subtree(subtree, None)
+        actual_tree.add_subtree(actual_subtree, None)
 
         expected_tree.add_subtree(exp_subtree, None)
 
